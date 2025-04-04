5f723a5361b28e4e659f6c4ee177f7a617811a00
garvps <gpasingh@ucdavis.edu>
2025-04-03 23:51:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+"""
+Flask App: Internship Listings Viewer
+
+This web application reads job listing data from a local SQLite database (jobs.db) and serves a webpage showing 
+all the scrapped software engineering internship positions. The listings are fetched using a Selenium scraper and 
+stored in the database before to running this app.
+"""
+
+# Import necessary libraries
 from flask import Flask, render_template
 import sqlite3
 
+# Initializing the Flask application
 app = Flask(__name__)
 
+# Define the homepage route
 @app.route("/")
 def home():
     conn = sqlite3.connect("jobs.db")
@@ -13,4 +24,5 @@ def home():
     return render_template("index.html", jobs=jobs)
 
 if __name__ == "__main__":
+    # Enable debug mode for hot reload and error messages
     app.run(debug=True)
